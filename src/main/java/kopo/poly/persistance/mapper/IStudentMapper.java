cd3832b80ce78e214ac67e28cdbe1e41631791a9
SeongukCho <tjddnr377@naver.com>
2023-06-13 16:47:42 +0900

    0613

--- src/main/java/kopo/poly/persistance/mapper/IStudentMapper.java
+++ src/main/java/kopo/poly/persistance/mapper/IStudentMapper.java
@@ -13,7 +13,6 @@ public interface IStudentMapper {
 //    함수명 : insertStudent -> Mapper XML 파일의 ID와 동일하며, 여러 개 작성가능한 SQL 쿼리를 구분하기 위한 값.
 //    Parameter : StudentDTO pDTO -> SQL 실행에 필요한 파라미터가 존재하는 객체로 Mapper XML 파일의 ParameterType과 동일
 //    리턴타입 : void -> insert 함수 실행한 뒤, 조회된 결과가 없음을 의미
-//
 //    @param pDTO 등록할 학생정보
 
     void insertStudent(StudentDTO pDTO) throws Exception;
